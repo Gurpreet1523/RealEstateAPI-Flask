375056994a2addbbecc184dba392e2a0843e3b8c
Gurpreet1523 <134833888+Gurpreet1523@users.noreply.github.com>
2024-10-21 14:21:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://@localhost\\SQLEXPRESS/realEstateDB?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'DIVEK@2007'     #os.urandom(24)
+    JWT_SECRET_KEY = os.urandom(24)
     #print(JWT_SECRET_KEY) # Randomly generated secret key
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 
